3322034721681984d7b1ae201a39a2667eeead89
Marko Avlijaš <marko.avlijas@gmail.com>
2020-07-18 12:55:10 +0200

    Skip creating ssh keys if they already exist

--- install/components/create_ssh_keys.sh
+++ install/components/create_ssh_keys.sh
@@ -1,16 +1,31 @@
 #! /bin/bash
 
-# load common.sh if it isn't loaded
-[ -z "$DOTFILES_DIR" ] && source "$(dirname "$0")/common.sh"
-
 # Create ssh keys with empty passphrase for github and bitbucket
+# Does not create keys if they exist
 
-info_msg "Creating github_rsa and bitbucket_rsa ssh keys\n"
+# load common.sh if it isn't loaded
+[ -z "$DOTFILES_DIR_ABS" ] && source "$(dirname "$0")/common.sh"
 
 my_git_user_email="$(git config user.email)"
 
-ssh-keygen -t rsa -b 4096 -C "$my_git_user_email" -N "" -f "$HOME/.ssh/github_rsa"
-ssh-keygen -t rsa -b 4096 -C "$my_git_user_email" -N "" -f "$HOME/.ssh/bitbucket_rsa"
+# Creates rsa key in "$HOME/.ssh/$1"
+create_ssh_key () {
+  if [ ! -f "$1" ]; then
+    ssh-keygen -t rsa -b 4096 -C "$my_git_user_email" -N "" -f "$1"
+    
+    if [ -f "$1" ]; then
+      success_msg "$1"
+    else
+      fail_msg_and_exit "$1"
+    fi
+  else
+    info_msg "Skipping '$1' (already exists)"
+  fi
+}
 
-success_msg "github_rsa and bitbucket_rsa\n"
+info_msg "Creating github_rsa and bitbucket_rsa ssh keys\n"
+
+create_ssh_key "$HOME/.ssh/github_rsa"
+create_ssh_key "$HOME/.ssh/bitbucket_rsa"
+
 

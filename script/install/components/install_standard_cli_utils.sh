e716e56a2aba08f1cf1b82c81393cb8ae88c420f
Marko Avlijaš <marko.avlijas@gmail.com>
2020-07-31 10:18:21 +0200

    Install htop

--- script/install/components/install_standard_cli_utils.sh
+++ script/install/components/install_standard_cli_utils.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-standard_cli_utils="wget curl tree xclip"
+standard_cli_utils="wget curl tree xclip htop"
 
 # load common.sh if it isn't loaded
 [ -z "$DOTFILES_DIR_ABS" ] && source "$(dirname "$0")/common.sh"

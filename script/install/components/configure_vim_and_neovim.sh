1f24b76afb4bada23b3427bde14e625fddc6e11a
Marko Avlijaš <marko.avlijas@gmail.com>
2020-08-01 15:18:21 +0200

    Set nvim as default editor and vi

--- script/install/components/configure_vim_and_neovim.sh
+++ script/install/components/configure_vim_and_neovim.sh
@@ -3,13 +3,32 @@
 # load common.sh if it isn't loaded
 [ -z "$DOTFILES_DIR_ABS" ] && source "$(dirname "$0")/common.sh"
 
+# Configure vim
 info_msg "Configuring vim"
 ln -sfv "$DOTFILES_DIR_REL_FROM_HOME/config/vim/vimrc" "$HOME/.vimrc"
 success_msg "Configure vim"
 
+# Configure neovim
 info_msg "Configuring neovim"
 mkdir -p "$HOME/.config/nvim"
 ln -sfv "../../$DOTFILES_DIR_REL_FROM_HOME/config/neovim/init.vim" \
         "$HOME/.config/nvim/init.vim"
 success_msg "Configure neovim"
 
+# set nvim as default editor
+info_msg "Setting nvim as default editor"
+
+nvim_path=$(which nvim)
+if [ -z "$nvim_path" ]; then
+  fail_msg_and_exit "Can't find nvim"
+fi
+
+# update-alternatives - maintain symbolic links determining default commands on Debian based systems
+#                                  link            name   path         priority 
+sudo update-alternatives --install /usr/bin/editor editor "$nvim_path" 1
+sudo update-alternatives --set editor "$nvim_path"
+
+sudo update-alternatives --install /usr/bin/vi vi "$nvim_path" 1
+sudo update-alternatives --set vi "$nvim_path"
+
+success_msg "Set nvim as default editor"

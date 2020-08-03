f6cdf38fb4dfa4fed1a53a1b9d839153ab1ddbb8
Marko Avlijaš <marko.avlijas@gmail.com>
2020-08-03 22:41:54 +0200

    Install fd and rg

--- script/install/components/install_cli_utils.sh
+++ script/install/components/install_cli_utils.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-standard_cli_utils="wget curl tree xclip htop"
+standard_cli_utils="wget curl tree xclip htop fd-find ripgrep"
 
 # load common.sh if it isn't loaded
 [ -z "$DOTFILES_DIR_ABS" ] && source "$(dirname "$0")/common.sh"
@@ -8,5 +8,13 @@ standard_cli_utils="wget curl tree xclip htop"
 info_msg "Installing standard cli utilities:\n$standard_cli_utils"
 
 sudo apt -y install $standard_cli_utils
+
+# for ubuntu only because it installs packages with weird names
+fdfind_path="$(which fdfind)"
+if [ -z fdfind_path ]; then
+  sudo ln -sv "fd" "$fdfind_path"
+else
+  fail_msg_and_exit "fdfind installation failed"
+fi
 
 success_msg "$standard_cli_utils"

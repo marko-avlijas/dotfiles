bcf39050aab8fc5b4980dfa407ef15eff600708b
Marko Avlijaš <marko.avlijas@gmail.com>
2020-08-07 14:31:04 +0200

    Don't install fd and ripgrep from package manager

--- script/install/components/install_cli_utils.sh
+++ script/install/components/install_cli_utils.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-standard_cli_utils="wget curl tree xclip htop fd-find ripgrep"
+standard_cli_utils="wget curl tree xclip htop"
 
 # load common.sh if it isn't loaded
 [ -z "$DOTFILES_DIR_ABS" ] && source "$(dirname "$0")/common.sh"
@@ -8,13 +8,5 @@ standard_cli_utils="wget curl tree xclip htop fd-find ripgrep"
 info_msg "Installing standard cli utilities:\n$standard_cli_utils"
 
 distro_package_manager_install $standard_cli_utils
-
-# for ubuntu only because it installs packages with weird names
-fdfind_path="$(which fdfind)"
-if [ -z $fdfind_path ]; then
-  sudo ln -sv "fd" "$fdfind_path"
-else
-  fail_msg_and_exit "fdfind installation failed"
-fi
 
 success_msg "$standard_cli_utils"

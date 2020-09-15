b77a032a110862410aa9df916f8f3599577c5251
Marko Avlijaš <marko.avlijas@gmail.com>
2020-09-15 09:47:49 +0200

    Create postgresql user after installing postgresql

--- script/install/install_databases.sh
+++ script/install/install_databases.sh
@@ -4,5 +4,14 @@
 [ -z "$DOTFILES_DIR_ABS" ] && source "$(dirname "$(dirname "$BASH_SOURCE")")/lib/all.sh"
 
 info_msg "Installing postgresql and sqlite3"
+
 distro_package_manager_install sqlite3 libsqlite3-dev postgresql libpq-dev postgresql-doc
+
 success_msg "Install postgresql and sqlite3"
+
+
+info_msg "Creating default user"
+
+sudo -u postgres createuser --superuser $(whoami)
+
+success_msg "Create default user"

bce2300b843f6ab73d66632535f44dd6e1d7bac3
Marko Avlijaš <marko.avlijas@gmail.com>
2020-09-15 07:11:03 +0200

    Use nvm to install node

--- config/shell/bash_and_zsh.sh
+++ config/shell/bash_and_zsh.sh
@@ -4,3 +4,7 @@ export EDITOR='vi' # is symlinked to nvim, check out configure nvim & vim script
 source /usr/local/share/chruby/chruby.sh
 # Make chruby automatically change ruby version as you cd into project
 source /usr/local/share/chruby/auto.sh
+
+# Enable nvm
+export NVM_DIR="$HOME/src/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
--- script/install/install_node.sh
+++ script/install/install_node.sh
@@ -3,8 +3,19 @@
 # load script/lib/all.sh if it isn't loaded
 [ -z "$DOTFILES_DIR_ABS" ] && source "$(dirname "$(dirname "$BASH_SOURCE")")/lib/all.sh"
 
+info_msg "Installing nvm"
+
+export NVM_DIR="$DOTFILES_SRC_DIR/nvm" && (
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+success_msg "Install nvm"
+
+
 info_msg "Installing node"
 
-distro_package_manager_install nodejs
+nvm install node
 
 success_msg "Install node"
